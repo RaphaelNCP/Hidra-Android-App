47a488c7b9862e35e74b937bb7d4cedccf1376a2
Raphael Costa <raphael.ofc11@gmail.com>
2025-01-14 21:47:16 -0300

    update

--- app/src/main/java/br/com/project/appdeteste/ui/MainActivity.kt
+++ app/src/main/java/br/com/project/appdeteste/ui/MainActivity.kt
@@ -1,11 +1,10 @@
-package br.com.project.appdeteste
+package br.com.project.appdeteste.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -24,13 +23,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color.Companion.Black
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import br.com.project.appdeteste.ui.navigation.ScreenItem
 import br.com.project.appdeteste.ui.screens.AddScreen
 import br.com.project.appdeteste.ui.screens.HomeScreen
@@ -54,8 +49,8 @@ class MainActivity : ComponentActivity() {
 fun App(modifier: Modifier = Modifier) {
     val screens = remember {
         listOf(
-            ScreenItem.Home,
             ScreenItem.Add,
+            ScreenItem.Home,
             ScreenItem.Settings,
         )
     }
--- app/src/main/java/br/com/project/appdeteste/ui/screens/HomeScreen.kt
+++ app/src/main/java/br/com/project/appdeteste/ui/screens/HomeScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -27,4 +28,10 @@ fun HomeScreen(modifier: Modifier = Modifier) {
 
         )
     }
+}
+
+@Preview
+@Composable
+private fun HomeScreenPreview() {
+    HomeScreen()
 }
